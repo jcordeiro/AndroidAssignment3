27f82be59e5e89fe631ae77b873644fcfedfd466
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-03-04 21:10:11 -0500

    Added PaymentActivity

---
+++ src/com/example/jonathan_cordeiro_3/PaymentActivity.java
@@ -0,0 +1,15 @@
+package com.example.jonathan_cordeiro_3;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PaymentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_payment);
+	}
+
+}
