76db8fb28ad9d596473597f97f6450feeb4dea65
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-03-06 17:39:05 -0500

    Implemented displayPizzaToppings() in PaymentActivity

--- src/com/example/jonathan_cordeiro_3/PaymentActivity.java
+++ src/com/example/jonathan_cordeiro_3/PaymentActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class PaymentActivity extends Activity {
 
@@ -15,6 +18,32 @@ public class PaymentActivity extends Activity {
 		Intent intent = getIntent();
 		Pizza pizza = intent.getParcelableExtra("pizza");
 		
+		
+		
+		
+	}
+	
+	public void displayPizzaToppings(Pizza pizza) {
+		
+		// Get a reference to the toppings textview widget
+		TextView txtPizzaToppings = (TextView)findViewById(R.id.txtPizzaTopings);
+		
+		StringBuilder toppings = new StringBuilder();
+		
+		// Loop through the pizza's toppings and append them to the string builder
+		for (String topping : pizza.getToppings()) {
+			toppings.append(topping + "\n");
+		}
+			
+		// Diplay the pizza toppings to the user
+		txtPizzaToppings.setText(toppings.toString());
+		
+		
+		
+	}
+	
+	
+	public void OnPayNow(View view) {
 		
 		
 		
